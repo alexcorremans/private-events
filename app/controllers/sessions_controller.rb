bc6f12af2f17abc695d57570581ab44f2e708422
Alexandra Corremans <alexandra.corremans@gmail.com>
2019-02-23 12:11:06 +1100

    add sessions controller - login and logout functionality

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by(name: params[:session][:name])
+    if @user
+      session[:user_id] = @user.id
+      flash[:success] = "Successfully logged in."
+      redirect_to root_url
+    else
+      flash.now[:error] = "User not found. Try again"
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    @current_user = nil
+    redirect_to root_url
+  end
+end
