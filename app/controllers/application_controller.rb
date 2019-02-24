de33712f7d9f56aaac3b96c3a76e09f2331ec33e
Alexandra Corremans <alexandra.corremans@gmail.com>
2019-02-24 14:33:15 +1100

    add before filters requiring login for restricted pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
+  before_action :require_login
+
+  private
+
+  def require_login
+    unless logged_in?
+      flash[:error] = "You must be logged in to access this page"
+      redirect_to login_path
+    end
+  end
 end
