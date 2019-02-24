fb5d31267ff6c060090608b45ad89ad9fae9b6b2
Alexandra Corremans <alexandra.corremans@gmail.com>
2019-02-24 14:24:03 +1100

    - configure event routes - add new event form - create shared error messages partial - clean up new user form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,27 @@
 class EventsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = current_user.created_events.build(event_params)
+    if @event.save
+      flash[:success] = "Event created successfully."
+      redirect_to root_url
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :date, :location, :description)
+  end
 end
