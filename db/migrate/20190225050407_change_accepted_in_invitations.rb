1412fbbff1b2d1a47cad2da502704ce841def1ba
Alexandra Corremans <alexandra.corremans@gmail.com>
2019-03-26 11:39:45 +1100

    fix migration updating 'accepted' column type to support PG

--- db/migrate/20190225050407_change_accepted_in_invitations.rb
+++ db/migrate/20190225050407_change_accepted_in_invitations.rb
@@ -1,9 +1,11 @@
 class ChangeAcceptedInInvitations < ActiveRecord::Migration[5.2]
   def up
+    change_column_default :invitations, :accepted, nil
     change_column :invitations, :accepted, :boolean, default: false, using: 'accepted::boolean'
   end
 
   def down
+    change_column_default :invitations, :accepted, nil
     change_column :invitations, :accepted, :string, default: "pending"
   end
 end
